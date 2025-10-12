17a9ff7835c8415d968453a5acc5a3907c1e4da4
FHUFRANKO <fhufrankostrona@op.pl>
2025-10-12 14:32:28 +0000

    fix(build): remove leftover ./live/* reference and restore default index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import AdsGrid from './live/AdsGrid';
-import AdDetails from './live/AdDetails';
-
 const root = createRoot(document.getElementById('root'));
-const path = window.location.pathname;
-const isList = path === '/' || path.startsWith('/ogloszenia');
-const isDetails = /^\/ogloszenie\/\d+\/?$/.test(path);
-
-root.render(
-  <React.StrictMode>
-    {isDetails ? <AdDetails /> : isList ? <AdsGrid /> : <App />}
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App /></React.StrictMode>);
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import AdsGrid from './live/AdsGrid';
-import AdDetails from './live/AdDetails';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-
-const path = window.location.pathname;
-const isList = path === '/' || path.startsWith('/ogloszenia');
-const isDetails = /^\/ogloszenie\/\d+\/?$/.test(path);
-
-root.render(
-  <React.StrictMode>
-    {isDetails ? <AdDetails /> : isList ? <AdsGrid /> : <App />}
-  </React.StrictMode>
-);
+root.render(<React.StrictMode><App /></React.StrictMode>);
